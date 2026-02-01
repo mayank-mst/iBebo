741dbc80b730994b78142da065d31c4967cfd39f
Masuk Sharma <masuk1304@Masuks-MacBook-Air.local>
2026-02-01 17:18:00 +0530

    modified workflow

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: "export",
   distDir: "dist", // GitHub Pages serves from the 'docs' folder
-  basePath: "/iBebo", // Replace with your repository name
   images: {
     unoptimized: false, // GitHub Pages does not support Next.js image optimization
   },
